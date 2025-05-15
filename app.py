a517c4992e0ccc4b4e8e31151bcce344c63e7a1c
Pepa0114 <efrain_emiliano16@hotmail.com>
2025-05-15 12:58:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev_key')  # Añadir una clave secreta para los formularios
+# Añadir una clave secreta para los formularios
 db = SQLAlchemy(app)
 db.init_app(app)
 
